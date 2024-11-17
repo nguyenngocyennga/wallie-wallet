8434b88b20265999defe86595b82136322a18c2b
Nga Nguyen <nganguyen.ny@gmail.com>
2024-11-17 22:13:48 +0800

    Implement account selection dialog for transaction import; update import handling to associate transactions with selected accounts

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -27,7 +27,7 @@ const app = new Hono()
             }
 
             const defaultTo = new Date();
-            const defaultFrom = subDays(defaultTo, 100);
+            const defaultFrom = subDays(defaultTo, 365);
 
             const startDate = from 
                 ? parse(from, "yyyy-MM-dd", new Date()) 
